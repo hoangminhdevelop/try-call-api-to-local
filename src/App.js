900f914e8bcaf24b99b0ffbc019f3ecee42b59d5
Minh Trương <minhtruonghoang28@gmail.com>
2023-11-09 17:38:26 +0700

    init

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const getData = async () => {
+      const res = await fetch("http://localhost:3030");
+      const dataJson = await res.json();
+      setData(dataJson);
+    };
+    getData();
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>{data?.name}</p>
         <a
           className="App-link"
           href="https://reactjs.org"
